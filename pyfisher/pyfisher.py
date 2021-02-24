0973fa740079b6a2e3f6538f745fab6a31cad661
Rugved Pund <rugvedpund@gmail.com>
2021-02-24 16:06:46 -0600

    add shade for contour_plot_mltiple, del c_plt2

--- pyfisher/pyfisher.py
+++ pyfisher/pyfisher.py
@@ -109,24 +109,6 @@ def contour_plot(fisher,fiducials,fname,name='',add_marker=False,latex=True):
     fig.set_size_inches(3 + fig.get_size_inches()) 
     fig.savefig(fname)
 
-def contour_plot2(fisher1, fisher2, fiducials,fname,name1='',name2='',add_marker=False,latex=True):
-    from chainconsumer import ChainConsumer
-    mean1 = [fiducials[key] for key in fisher1.params]
-    mean2 = [fiducials[key] for key in fisher2.params]
-    cov1 = np.linalg.inv(fisher1.values)
-    cov2 = np.linalg.inv(fisher2.values)
-    parameters1 = [latex_mapping[x] for x in fisher1.params] if latex else fisher1.params
-    parameters2 = [latex_mapping[x] for x in fisher2.params] if latex else fisher2.params
-
-    c = ChainConsumer()
-    c.add_covariance(mean1, cov1, parameters=parameters1, name=name1,shade=False)
-    c.add_covariance(mean2, cov2, parameters=parameters2, name=name2,shade=False)
-    if add_marker: c.add_marker(mean1, parameters=parameters1, marker_style="*", marker_size=100, color="r",name='')
-    c.configure(usetex=False, serif=False,sigma2d=True,sigmas=[1])
-    fig = c.plotter.plot()
-    fig.set_size_inches(3 + fig.get_size_inches()) 
-    fig.savefig(fname)
-
 def contour_plot_multiple(fisher_list,fiducials,fname,name_list,add_marker=False,latex=True):
     from chainconsumer import ChainConsumer
     c = ChainConsumer()
@@ -137,7 +119,7 @@ def contour_plot_multiple(fisher_list,fiducials,fname,name_list,add_marker=False
         means_list.append([fiducials[key] for key in fisher_list[i].params])
         covs_list.append(np.linalg.inv(fisher_list[i].values)) 
         parameters_list.append([latex_mapping[x] for x in fisher_list[i].params] if latex else fisher_list[i].params)
-        c.add_covariance(means_list[i], covs_list[i], parameters=parameters_list[i], name=name_list[i],shade=False)
+        c.add_covariance(means_list[i], covs_list[i], parameters=parameters_list[i], name=name_list[i])
     
     if add_marker: c.add_marker(mean1, parameters=parameters1, marker_style="*", marker_size=100, color="r",name='')
     c.configure(usetex=False, serif=False,sigma2d=True,sigmas=[0,1,2])
